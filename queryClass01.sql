0586ca716a2b43579e2eefd8406f00e82a050a54
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-04-21 06:10:55 -0400

    First commit to MySQL Classes

---
+++ queryClass01.sql
@@ -0,0 +1,44 @@
+//
+// Class Nro. 1
+
+CREATE DATABASE test CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE test;
+
+CREATE TABLE members (
+	IDNumbers  INT Primary key,
+	LastName   VARCHAR(30),
+	FirstName  VARCHAR(30),
+	Email      VARCHAR(50),
+	MemberSince 	DATE
+);
+
+INSERT INTO members (IDNumbers, LastName, FirstName, Email, MemberSince) VALUES();
+
+//
+// Class Nro. 2
+
+// Part Nro. 1
+
+// Part Nro. 2
+
+
+//
+// Class Nro. 3
+
+// Part Nro. 1
+
+// Part Nro. 2
+
+// Part Nro. 3
+
+
+
+
+//
+// Class Nro. 4
+
+
+
+//
+// Class Nro. 5
