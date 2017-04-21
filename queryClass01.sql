031f2aecf8857e2aaec011118e88218f071daa16
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-04-21 06:50:30 -0400

    Add INNER JOIN examples

--- queryClass01.sql
+++ queryClass01.sql
@@ -31,6 +31,28 @@ INSERT INTO members (IDNumbers, LastName, FirstName, Email, MemberSince) VALUES(
 // Part Nro. 2
 
 // Part Nro. 3
+//
+// Inner Join
+
+// Single Join
+SELECT t1.emp_no, 
+		t1.first_name, 
+		t1.last_name, 
+		t2.title, 
+		t2.from_date, 
+		t2.to_date 
+	from employees as t1 
+	INNER JOIN titles as t2 ON t1.emp_no = t2.emp_no;
+
+// Three tables Join with header 
+SELECT t1.emp_no COD,
+		 t1.first_name NOMBRE, 
+		 t1.last_name APELLIDO, 
+		 t3.dept_name DEPARTAMENT, 
+		 t2.from_date 
+	from employees as t1 
+	inner join dept_manager as t2 on t1.emp_no = t2.emp_no 
+	inner join departments as t3 on t2.dept_no = t3.dept_no;
 
 
 
